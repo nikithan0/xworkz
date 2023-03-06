73e2aae631dc3020a82b1e3b7450a361a4616e49
nikithan0 <nikithan58.xworkz@gmail.com>
2023-03-06 22:11:57 +0530

    second-code-push

--- ArrayLoop.java
+++ ArrayLoop.java
@@ -1,18 +1,76 @@
 public class ArrayLoop {
 	public static void main(String[] arg){
-		int[] registerNumbers = {2011001,1011002,3011003,3011005,4011005,3011006,7011008,9011364};
-		long[] vehicleNumbers = {201452L,201565L,201179L,201389L,201953L,202388L,202377L,201199};
-		float[] heightOfHuman = {3.6510F,6.0000F,6.1200F,5.7512F,4.1430F,4.1200F,6.5200F,5.4200F};
-		double[] percentageInSujects ={55.5400D,72.77D,85.78D,89.10D,99.76D,94.12D,80.35D,68.66D} ;
-		boolean[] statements = {true,true,false,true,false,true,false,true};
-        char[] anyAlphabets = {'A', 'G', 'N', 'I', 'P', 'J', 'D', 'L' };
+		int[] registerNumbers = {2011001,1011002,3011003,3011005};
+		int[] carNumbers = {10221,10222,10223,10224};
+		int[] registerNmber = {103,105,107,108};
+		int[] classNumber = {601,602,603,604};
+		int[] roomNumber = {110,112,113,116};
+		int[] rollNumber = {901,902,903,904};
+		int[] anyNumbers = {701,901,1000,189};
+		int[] laptopNumbers = {655,689,595,440,349};
+		int[] systems = {889,2754,245,254};
+		int[] countingFrom = {1000,2000,3000,4000};
 		
-		for(int i=0; i<=7; i++){
+		long[] vehicleNumbers = {201452L,201565L,201179L,201389L,202388L,201199L};
+		long[] peoplesCount = {2000L,3000L,8000L,9000L,10000L,7000L};
+		long[] salary = {10000L,20000L,30000L,40000L,50000L,60000};
+		long[] animalsCount = {40000L,60000L,80000L,870000L,843900,10000L};
+		long[] weightOfElephant = {5000L,5500L,7900L,8399L,3899L,3999L};
+		long[] heightOfbuildings = {4000L,8049L,3388L,9000L,3758L};
+		long[] heightOfTree = {80000L,7000L,7600L,4000L,};
+		long[] weightWillBe = {70000L,80000L,75000L};
+		long[] countOfPlants = {6000L,86000L,79398L,47834};
+		long[] thousands = {11000L,892032L,87590L,90000L};
+		
+		float[] heightOfHuman = {3.6510F,6.0000F,6.1200F,5.7512F,4.1430F};
+		float[] waterPercentage = {50.77F,87.00F,89.89L};
+		float[] fruitPercentage = {89.99F,79.00F,75,00F};
+		float[] icePercentage = {67.00F,8.99F,7.00F,87.00F};
+		float[] kilometers = {1.00F,2.00F,4.77F,78.99F};
+		float[] distance = {2.88F,9.00F,7.88F};
+		float[] anyDecimalNumber = {89.00F,6.99F,78.88F};
+		float[] heightOfTrees = {8.99F,10.00F,78.00F};
+		float[] rupees = {29.00F,70.77F,60.88F};
+		
+		double[] percentageInSujects ={55.54D,72.77D,85.78D,89.10D,} ;
+		double[] indianInr = {79.99D,80.77D,88.88D};
+		double[] rank = {69.77D,66.78D,99.99D};
+		double[] grade = {88.00D,89.89D,89.87D};
+		double[] permitivity = {8.859D,8.84D,8.83D};
+		double[] plancksConstant = {6.625D,6.624D,6.626D};
+		double[] feverTemperature = {98.6D,96.7D,96,55D};
+		double[] valuOfPi = {3.142D,3.141D,3.143D};
+		double[] valueOfGravitationalConstant = {6.674D,6.683D,6.687D};
+		double[] valueOfElectronCharge = {1.602D,1.603D,1.612D};
+		
+		boolean[] statements = {true,true,false,true,false,true,false,true};
+		boolean[] isThereAnyPasasite = {true,false,true,false};
+		boolean[] marsIsPlanet = {true,true,false,false};
+		boolean[] webIsApplicaion = {true,false,true,false};
+		boolean[] androidIsIos = {true,false,false,true};
+		boolean[] laptopIsDevice = {false,true,true,false};
+		boolean[] bloodContainsRbc = {true,false,false,true};
+		boolean[] humanBodyHasAntibodies {true,false,true,false};
+		boolean[] animalNeedOxygen = {true,false,true,false};
+		boolean[] ironPresentInBlood = {true,true,false,false};
+		
+        char[] anyAlphabets = {'A', 'G', 'N', 'I', 'P', 'J', 'D', 'L' };
+		char[] grades = {'A','B','C'};
+		char[] vitamins = {'A','B','C','D'};
+		char[] questionMains = {'A','B','C'};
+		char[] nameInCapitals = {'N','A','M','E'};
+		char[] anotherForMe = {'I','I'};
+		char[] exampleForVariablesInMaths = {'X','x','Y','y'};
+		char[] bag = {'B', 'A', 'G'};
+		char[] alphabets = {'A','L','P','H','A','B','E','T','S'};
+		char[] thirdLineInKeyBoard = {'x','c','v','b','n','m'};
+		
+		for(int i=0; i<=3; i++){
 			System.out.println(registerNumbers[i]);
 		};
 		
-    System.out.println(vehicleNumbers[7]);
-	System.out.println(heightOfHuman[6]);
+    System.out.println(vehicleNumbers[4]);
+	System.out.println(heightOfHuman[3]);
 	System.out.println(percentageInSujects[3]);
 	System.out.println(statements[3]);
 	System.out.println(anyAlphabets[5]);
