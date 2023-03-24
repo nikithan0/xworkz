bab69ff72970f79bf48cb4db2d2e2672e2a947c7
nikithan0 <nikithan58.xworkz@gmail.com>
2023-03-24 10:03:23 +0530

    States n Behaviour

---
+++ Dog.java
@@ -0,0 +1,68 @@
+public class Dog{
+	final String breed = "Husky";
+	final String name = "Scooby";
+	final byte numbersOfLeg = 4;
+	final int ears = 2;
+	final byte tail = 1;
+	boolean itIsFriendly;
+	static byte belt;
+	static long teeth;
+	static String location;
+	static String colour;
+	static byte nails;
+	int price;
+	final String gender = "Male";
+	byte weight;
+	int height;
+	String colourOfEyes;
+	boolean isDangerous;
+	byte width;
+	String favoriteFood;
+	byte month;
+    	
+	
+	public static void main(String[] args){
+		
+	}
+		
+		public void faithfulCompanion(){
+			
+		} 
+		
+		public void protectiveToOwner(){
+			
+		}
+		
+		public void goodAndLoyalFriend(){
+			
+		}
+	
+	    public void tailChasing(){
+			
+		}
+		
+		public void chasesOtherDogs(){
+			
+		}
+		
+		public void tiltingTheirHead(){
+			
+		}
+		
+		public void barking(){
+			
+		}
+		
+		public void dogGoForHuntingWithHumans(){
+			
+		}
+		
+		public void playfulPartner(){
+			
+		}
+		
+		public void bittingTheStrangers(){
+			
+		}
+		
+}
